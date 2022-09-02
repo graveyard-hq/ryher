1e9e519e43f2399e33cd287355f68ce6dece4f6f
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-09-02 08:57:56 +0000

    feat: added account controller

---
+++ backend/src/controllers/account/account.controller.ts
@@ -0,0 +1,52 @@
+import { Response, Request } from "express";
+
+async function createAccount(req: Request, res: Response) {
+  try {
+  } catch (error) {
+    res.status(500).send({
+      statusCode: 500,
+      message: "Internal Server Error",
+      payload: null,
+    });
+  }
+}
+
+async function getAccount(req: Request, res: Response) {
+  try {
+  } catch (error) {
+    res.status(500).send({
+      statusCode: 500,
+      message: "Internal Server Error",
+      payload: null,
+    });
+  }
+}
+
+async function updateAccount(req: Request, res: Response) {
+  try {
+  } catch (error) {
+    res.status(500).send({
+      statusCode: 500,
+      message: "Internal Server Error",
+      payload: null,
+    });
+  }
+}
+
+async function deleteAccount(req: Request, res: Response) {
+  try {
+  } catch (error) {
+    res.status(500).send({
+      statusCode: 500,
+      message: "Internal Server Error",
+      payload: null,
+    });
+  }
+}
+
+export default {
+  createAccount,
+  getAccount,
+  updateAccount,
+  deleteAccount,
+};
