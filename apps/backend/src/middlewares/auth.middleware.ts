2d9e843b3235cb22dc3ff781072dc5fa400e8c0f
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-09-12 17:20:09 +0000

    remove logger from jwt

--- apps/backend/src/middlewares/auth.middleware.ts
+++ apps/backend/src/middlewares/auth.middleware.ts
@@ -10,17 +10,18 @@ declare module "express" {
 
 const authorizeRequest = async (req: Request, res: Response, next: NextFunction) => {
   let accessToken: any;
+
   if (req.headers.authorization && req.headers.authorization?.startsWith("Bearer")) {
     try {
+
       accessToken = req.headers.authorization?.split(" ")[1];
 
       const value = <any>jwt.verify(accessToken, process.env.JWT_SECRET_KEY as string);
+
       req.user = value;
 
       next();
     } catch (error) {
-      log.error(error);
-
       res.status(401).send({
         statusCode: 401,
         message: "Unauthorized",
