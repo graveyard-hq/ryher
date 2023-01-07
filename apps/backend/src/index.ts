1831f3b9c278dbf510cf0cf3336a4e45247e6949
Aelpxy <aelpxy@protonmail.com>
2023-01-07 16:11:40 +0000

    migrate(wip): fastify setup

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,27 @@
+import fastify, { FastifyInstance } from "fastify";
+import * as dotenv from "dotenv";
+import cors from "@fastify/cors";
+import helmet from "@fastify/helmet";
+import { fastifyYupSchema } from "fastify-yup-schema";
+
+dotenv.config();
+
+const app: FastifyInstance = fastify({
+  logger: true,
+});
+
+app.register(cors, { origin: process.env.ORIGIN || "*" });
+app.register(helmet);
+app.register(fastifyYupSchema);
+
+app.register(() => {}, { prefix: "/" });
+
+const start = async (port: number) => {
+  try {
+    app.listen({ port: port, host: "0.0.0.0" });
+  } catch (error) {
+    process.exit(1);
+  }
+};
+
+start(Number(process.env.PORT) || 8080);
