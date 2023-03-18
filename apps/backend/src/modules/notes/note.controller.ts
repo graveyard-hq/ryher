1b5d4c4ae7e4fcd976b05558d7cb913b25b38ff8
Aelpxy <aelpxy@protonmail.com>
2023-03-18 19:39:02 +0600

    chore(cleaup): just use nextjs

--- apps/backend/src/modules/notes/note.controller.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyRequest, FastifyReply } from "fastify";
-
-const createNote = async (request: FastifyRequest<{}>, reply: FastifyReply): Promise<void> => {};
-const getNotes = async (request: FastifyRequest<{}>, reply: FastifyReply): Promise<void> => {};
-const getNote = async (request: FastifyRequest<{}>, reply: FastifyReply): Promise<void> => {};
-const updateNote = async (request: FastifyRequest<{}>, reply: FastifyReply): Promise<void> => {};
-
-export { createNote, getNotes, getNote, updateNote };
